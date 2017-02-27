190442b43392d4dda63ca6576eec4a8a9148fdbe
Máté Farkas <farkas.mate38@gmail.com>
2017-02-27 23:40:34 +0100

    use strict mode

--- cli.js
+++ cli.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+
+'use strict';
+
 const meow = require('meow');
 const zimap = require('./');
 
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const fs = require('fs-promise');
 
 const indicesRegex = /z-index\s*:\s*(\-?\d+);?/g;
